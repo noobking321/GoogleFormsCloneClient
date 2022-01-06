349f5281b5c34ca2e998bb87a18532c7d3715b93
Leevesh Kamboj <leeveshk12@gmail.com>
2022-01-06 23:04:32 +0530

    small fix

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
   untitledFormName : "Untitled form",
   type_options : ["Short Answer", "Long Answer", "Dropdown"],
-  baseUrl : "https://google-form-clone.herokuapp.com"
+  // baseUrl : "https://google-form-clone.herokuapp.com"
+  baseUrl: ""
 };
 
 module.exports = config;
