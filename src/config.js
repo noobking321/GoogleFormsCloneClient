b14f2abc8fa8c1c14b72b73baf498f47223d3ba7
Leevesh Kamboj <leeveshk12@gmail.com>
2022-12-04 23:21:32 +0530

    backend url chamge

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  serverUrl: "https://google-form-clone.herokuapp.com",
+  serverUrl: "https://formserver.dexterpro.site",
   untitledFormName: "Untitled form",
   type_options: ["Short Answer", "Long Answer", "Dropdown", "MCQ"],
   quetionTypesWithOptions: [2, 3],
